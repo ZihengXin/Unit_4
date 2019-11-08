72d62392c2be706c6e5424458fe9b63524b4526b
Starmicks <ziheng.xin@ssfs.org>
2019-11-08 10:56:25 -0500

    Initial commit

---
+++ src/WhileLoops.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class WhileLoops {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static String myMethod(){
+        scan.nextInt();
+    }
+    public static void main(String[] args){
+
+    }
+}
