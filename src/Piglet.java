0804b4824e2e8ba709a8e0821d6c236021fa4958
Starmicks <ziheng.xin@ssfs.org>
2019-11-19 10:53:18 -0500

    11.19 piglet

--- src/Piglet.java
+++ src/Piglet.java
@@ -1,11 +1,29 @@
+//11.19 Charly. This program should be able to roll dices until you get a one.
 import java.util.Scanner;
 
 public class Piglet {
-    public static void main(String[] args) {
+
+    public static String game() {
         Scanner input = new Scanner(System.in);
-        int initial = (int) (Math.random() * 6);
-        while (initial > 1) {
-            System.out.println("Welcome to Piglet !");
+        System.out.println("Welcome to Piglet!");
+        int result = 0;
+        String answer = "yes";
+        while (answer.equals("yes")) {
+            int eachRoll = (int) (Math.random() * 6);
+            if (eachRoll == 0) {
+                result = 0;
+                System.out.println("You rolled a 1!");
+                return "You got 0 points.";
+            } else {
+                result += eachRoll;
+                System.out.println("You rolled a " + eachRoll + "!");
+                System.out.println("Roll again?");
+                answer = input.next();
+            }
         }
+        return "You got " + result + " points.";
+    }
+    public static void main(String[] args){
+        System.out.println(game());
     }
 }
