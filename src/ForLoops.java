dcf67e0a598ed3de9df8c55b5388fc09cd5fdc91
Starmicks <ziheng.xin@ssfs.org>
2019-11-19 18:47:28 -0500

    11.19 ForLoops with errors.

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,4 +1,4 @@
-// 11/19 Charly. For loops exercised. This program includes multiple methods for the test. 
+// 11/19 Charly. For loops exercised. This program includes multiple methods for the test.
 import java.util.Scanner;
 
 public class ForLoops {
@@ -16,10 +16,41 @@ public class ForLoops {
         String result = "";
         if (one<two)
             for(int num = two; num > one; num--){
-                result += num;
+                result += num + " ";
             }
         else
             result = "";
         return result;
+    }
+    public static String countDown2(int one, int two){
+        String result = "";
+        if (one<two){
+            for(int num = two; num > one; num--){
+                result += num + " ";
+            }}
+        else if (one>two){
+            for(int num = one; num > two; num--){
+                result += num + " ";
+            }}
+        else
+            result = "";
+        return result;
+    }
+    public static int sumNumbers(int one, int two){
+        int output = 0;
+        if (one < two){
+            for (int value = one; value < two; value++){
+                output += value;
+            } }
+        else if (one > two){
+            for (int value = two; value < one; value++){
+                output += value;
+            } }
+        else if (one == two){
+            output = one;
+        }
+        else
+            output = 0;
+        return output;
     }
 }
