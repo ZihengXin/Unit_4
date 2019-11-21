aa55b288e5fe7fdc23e90f52886082386100568c
Starmicks <ziheng.xin@ssfs.org>
2019-11-21 13:50:39 -0500

    11.21 forLoop done(pass the test)

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -7,17 +7,20 @@ public class ForLoops {
     }
     public static String printingHashTags(int num){
         String result = "";
-        for (int times = 1; times<num; times++){
+        for (int times = 1; times<=num; times++){
             result+="#";
         }
         return result;
     }
     public static String countDown(int one, int two){
         String result = "";
-        if (one<two)
-            for(int num = two; num > one; num--){
+        if (one<two){
+            for(int num = two; num >=one; num--)
                 result += num + " ";
+
             }
+        else if (one == two)
+            result = String.valueOf(two)+" ";
         else
             result = "";
         return result;
@@ -25,25 +28,25 @@ public class ForLoops {
     public static String countDown2(int one, int two){
         String result = "";
         if (one<two){
-            for(int num = two; num > one; num--){
+            for(int num = two; num >=one; num--){
                 result += num + " ";
             }}
         else if (one>two){
-            for(int num = one; num > two; num--){
+            for(int num = one; num >=two; num--){
                 result += num + " ";
             }}
         else
-            result = "";
+            result = String.valueOf(two)+" ";
         return result;
     }
     public static int sumNumbers(int one, int two){
         int output = 0;
         if (one < two){
-            for (int value = one; value < two; value++){
+            for (int value = one; value <= two; value++){
                 output += value;
             } }
         else if (one > two){
-            for (int value = two; value < one; value++){
+            for (int value = two; value <= one; value++){
                 output += value;
             } }
         else if (one == two){
