a5af55acfb268cd0b43a1c2f237de93623ff3606
Starmicks <ziheng.xin@ssfs.org>
2019-12-03 08:58:44 -0500

    12.3 NestedLoops first 2 done

---
+++ src/NestedLoops.java
@@ -0,0 +1,46 @@
+//12.3 Charly. This program should be able to pass through the NestedLoopsTest.
+import java.util.Scanner;
+
+public class NestedLoops {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+    }
+    public static String xSquare(){
+        String str = "";
+        for (int a = 1; a<6; a++){
+            for (int b = 1; b<6; b++){
+                str +="X";
+            }
+            str += "\n";
+        }
+        return str;
+    }
+    public static String xSquare2(int n){
+        String str = "";
+        for (int a = 1; a<= n; a++){
+            for (int b = 1; b<= n; b++){
+                str +="X";
+            }
+            str += "\n";
+        }
+        return str;
+    }
+    public static String flippedTriangle(int n){
+        String str = "";
+        for (int a = 1; a <= n; a++){
+            for (int b = n; b >= 1; b--){
+                str += "*";
+            }
+            str += "\n";
+        }
+        return str;
+    }
+    public static String multiplicationTable(){
+        String str = "";
+        for (int a = 1; a <= 9; a ++){
+
+        }
+        return str;
+    }
+}
